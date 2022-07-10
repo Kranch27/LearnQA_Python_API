6999d5bf4e03afc07b5da98df054ba32b448fc1f
KirillTihomirov <tihomirovks@ya.ru>
2022-07-10 19:52:19 +0300

    ex11

---
+++ Ex12.py
@@ -0,0 +1,7 @@
+import requests
+
+class TestEx12:
+
+    def test_ex12(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_header")
+        assert response.headers.get("x-secret-homework-header") == "Some secret value", "Некорректное значение!"
